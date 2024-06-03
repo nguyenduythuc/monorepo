a226ec663c954205ebd05d48da22f7536798360c
Thức Nguyễn Duy <thucnguyenduy@Thucs-iMac.local>
2024-06-03 11:05:33 +0700

    Init monorepo with iOS setup

---
+++ packages/mobile/metro.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+// Path to the root directory of your monorepo
+const rootPath = path.resolve(__dirname, '../../');
+
+// Watch folders for symlinks and hoisted node_modules
+const watchFolders = [
+  path.resolve(__dirname, 'node_modules'),
+  path.resolve(rootPath, 'node_modules'),
+  // Add other shared packages if needed
+//   path.resolve(rootPath, 'packages/shared'),
+];
+
+// Resolver settings
+const resolver = {
+  unstable_enableSymlinks: true, // Enable symlink support
+//   extraNodeModules: {
+//     // Redirect all react-native imports to the project node_modules
+//     'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+//   },
+};
+
+const config = {
+  watchFolders,
+  resolver,
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
