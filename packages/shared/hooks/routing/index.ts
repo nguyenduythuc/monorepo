02fae11bf13047b9eca39d926674f9b20af42921
hwngvux98 <hwngvux98@gmail.com>
2024-08-05 15:38:38 +0700

    [issue-24] fix types error

--- packages/shared/hooks/routing/index.ts
+++ packages/shared/hooks/routing/index.ts
@@ -1,10 +1,20 @@
 import {Platform} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import {
+  useNavigation,
+  NavigationProp,
+  NavigationAction,
+} from '@react-navigation/native';
 import {PrimaryNavigatorNavigationProp} from '../../../mobile/src/navigators/RootNavigator';
 import {RootParamList} from '../../../mobile/src/types/paramtypes';
+import {Dispatch} from 'redux'; // Assuming you're using redux
 
+interface UseNavigationReturn {
+  dispatch: Dispatch<NavigationAction>;
+  navigate: (routeName: string, params?: object) => any;
+}
 export const useConfigRouting = () => {
-  const {dispatch, navigate} = useNavigation();
+  const {dispatch, navigate}: UseNavigationReturn = useNavigation();
+
   const appNavigate = (nextScreen: string, params?: object) => {
     console.log('nextScreenWeb', nextScreen);
     dispatch(navigate(nextScreen, params));
