6625fb9d4f781f986e4ae05f9fa425a1ecd6c3b3
Thức Nguyễn Duy <thucnguyenduy@Thucs-iMac.local>
2024-08-13 16:08:48 +0700

    [issue-50] Proxy config for web

--- packages/shared/redux/slices/apiSlices/authAPI.ts
+++ packages/shared/redux/slices/apiSlices/authAPI.ts
@@ -1,11 +1,12 @@
 import {BaseQueryFn, EndpointBuilder} from '@reduxjs/toolkit/query';
 import {GetAccountResponseProps} from '@lfvn-customer/shared/types/services/authTypes';
+import { getPath } from './config';
 export const authAPI = (
   builder: EndpointBuilder<BaseQueryFn, never, 'LFVN-API'>,
 ) => ({
   getAccount: builder.mutation<GetAccountResponseProps, void>({
     query: () => ({
-      url: '/api/account',
+      url: getPath('/api/account'),
       method: 'get',
     }),
   }),
--- packages/shared/redux/slices/apiSlices/productAPI.ts
+++ packages/shared/redux/slices/apiSlices/productAPI.ts
@@ -5,13 +5,14 @@ import {
   ProductResponseProps,
   PurposeResponseProps,
 } from '@lfvn-customer/shared/types/services/productTypes';
+import { getPath } from './config';
 
 export const productAPI = (
   builder: EndpointBuilder<BaseQueryFn, never, 'LFVN-API'>,
 ) => ({
   getProductList: builder.query<ProductGroupProps, void>({
     query: () => ({
-      url: '/api/product-group',
+      url: getPath('/api/product-group'),
       method: 'get',
     }),
   }),
