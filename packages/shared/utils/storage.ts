fc41e79ef4037c1b05bb3250fa72537ea4cb416a
Thức Nguyễn Duy <thucnguyenduy@Thucs-iMac.local>
2024-09-19 10:33:04 +0700

    Ignore mmkv storage on web

--- packages/shared/utils/storage.ts
+++ packages/shared/utils/storage.ts
@@ -5,14 +5,23 @@ export const storage = new MMKV();
 
 export const mmkvStorage: Storage = {
   setItem: (key, value) => {
+    if (typeof window === 'undefined') {
+      return Promise.resolve();
+    }
     storage.set(key, value);
     return Promise.resolve(true);
   },
   getItem: key => {
+    if (typeof window === 'undefined') {
+      return Promise.resolve();
+    }
     const value = storage.getString(key);
     return Promise.resolve(value);
   },
   removeItem: key => {
+    if (typeof window === 'undefined') {
+      return Promise.resolve();
+    }
     storage.delete(key);
     return Promise.resolve();
   },
